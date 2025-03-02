ba0c349c7ab14062e3324869d7b48df948d77f7a
ba41ra <egormelyh38@gmail.com>
2025-03-02 18:38:21 +0300

    Add files via upload

---
+++ chipher.py
@@ -0,0 +1,18 @@
+ALPHABET_RU = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+ALPHABET_EN = "abcdefghijklmnopqrstuvwxyz"
+lang = input("Выберите язык - RU/EN:")
+if lang =="RU":
+    alphabet = ALPHABET_RU
+else:
+    alphabet = ALPHABET_EN
+message = input("Введите сообщение:").lower()
+print(message)
+result = ""
+chipher = []
+step = int(input("Введите шаг сдвига:"))
+for symbol in message:
+    chipher.append(alphabet.find(symbol)+ step)
+print(f"Шифр: {chipher}")
+for symbol in chipher:
+    result += alphabet[symbol - step]
+print(result)
