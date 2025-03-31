1c95d7f4e32c2025cdf2f78a8ea4ec38ad253352
ba41ra <egormelyh38@gmail.com>
2025-03-31 12:32:50 +0300

    Update chipher.py

--- chipher.py
+++ chipher.py
@@ -1,12 +1,11 @@
-ALPHABET_RU = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-ALPHABET_EN = "abcdefghijklmnopqrstuvwxyz"
+ALPHABET_RU = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя "
+ALPHABET_EN = "abcdefghijklmnopqrstuvwxyz "
 lang = input("Выберите язык - RU/EN:")
 if lang =="RU" or lang =="ru":
     alphabet = ALPHABET_RU
 else:
     alphabet = ALPHABET_EN
 message = input("Введите сообщение:").lower()
-print(message)
 result = ""
 chipher = []
 step = int(input("Введите шаг сдвига:"))
@@ -15,4 +14,4 @@ for symbol in message:
 print(f"Шифр: {chipher}")
 for symbol in chipher:
     result += alphabet[symbol - step]
-print(result)
+print(f"Вы отправили: {result}")
